ff482cd23b3f7407b7b4c80a4883d5d0ec177540
Brodnevyo <127347881+Brodnevyo@users.noreply.github.com>
2023-04-07 16:17:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,8 +7,7 @@ chat_id = 1025787461 # Ваш chat ID, не меняйте название пе
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
-    n = len(x)
-    x_mean = np.mean(x)
-    x_var = np.mean(x**2) - x_mean**2
-    a = 2 * x_var / (79**2 * n)# Не меняйте название функции и её аргументы
+    x_bar = np.mean(x)
+    s = np.std(x, ddof=1)
+    a = (x_bar - 79) / (2 * s**2)
     return a # Ваш ответ
